09504cbb90f6b6c5c0c847b841a004c386328093
NishiyamaStudios <jcwasterberg@gmail.com>
2023-02-02 21:41:09 +0100

    Shopping with Firebase database and auth

--- app/src/main/java/se/nishiyamastudios/pia11feb02shopping/LoginFragment.kt
+++ app/src/main/java/se/nishiyamastudios/pia11feb02shopping/LoginFragment.kt
@@ -6,8 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,12 +27,41 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = Firebase.auth
+
         view.findViewById<Button>(R.id.loginButton).setOnClickListener {
+
+            val userEmail = view.findViewById<EditText>(R.id.loginEmailET).text.toString()
+            val userPassword = view.findViewById<EditText>(R.id.loginPasswordET).text.toString()
+
+            auth.signInWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(requireContext(), "Login ok.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(requireContext(), "Login failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
 
         }
 
         view.findViewById<Button>(R.id.registerButton).setOnClickListener {
 
+            val userEmail = view.findViewById<EditText>(R.id.loginEmailET).text.toString()
+            val userPassword = view.findViewById<EditText>(R.id.loginPasswordET).text.toString()
+
+            auth.createUserWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(requireContext(), "Register ok.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(requireContext(), "Register failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
 
     }
--- app/src/main/java/se/nishiyamastudios/pia11feb02shopping/MainActivity.kt
+++ app/src/main/java/se/nishiyamastudios/pia11feb02shopping/MainActivity.kt
@@ -3,15 +3,15 @@ package se.nishiyamastudios.pia11feb02shopping
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var isLoggedin = true
-
-        if(isLoggedin) {
+        if(Firebase.auth.currentUser != null) {
             supportFragmentManager.commit {
                 replace(R.id.mainFragCon, ShoppingFragment())
             }
