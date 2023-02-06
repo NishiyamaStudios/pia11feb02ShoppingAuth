08332be775f1799ae5dbfa6328aa78a2aef07206
NishiyamaStudios <jcwasterberg@gmail.com>
2023-02-06 22:26:03 +0100

    Shopping with Firebase database and auth

--- app/src/main/java/se/nishiyamastudios/pia11feb02shopping/LoginFragment.kt
+++ app/src/main/java/se/nishiyamastudios/pia11feb02shopping/LoginFragment.kt
@@ -11,17 +11,30 @@ import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import se.nishiyamastudios.pia11feb02shopping.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
+
+    var _binding : FragmentLoginBinding? = null
+    val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        //return inflater.inflate(R.layout.fragment_login, container, false)
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -29,10 +42,10 @@ class LoginFragment : Fragment() {
 
         auth = Firebase.auth
 
-        view.findViewById<Button>(R.id.loginButton).setOnClickListener {
+        binding.loginButton.setOnClickListener {
 
-            val userEmail = view.findViewById<EditText>(R.id.loginEmailET).text.toString()
-            val userPassword = view.findViewById<EditText>(R.id.loginPasswordET).text.toString()
+            val userEmail = binding.loginEmailET.text.toString()
+            val userPassword = binding.loginPasswordET.text.toString()
 
             auth.signInWithEmailAndPassword(userEmail, userPassword)
                 .addOnCompleteListener(requireActivity()) { task ->
@@ -47,10 +60,10 @@ class LoginFragment : Fragment() {
 
         }
 
-        view.findViewById<Button>(R.id.registerButton).setOnClickListener {
+        binding.registerButton.setOnClickListener {
 
-            val userEmail = view.findViewById<EditText>(R.id.loginEmailET).text.toString()
-            val userPassword = view.findViewById<EditText>(R.id.loginPasswordET).text.toString()
+            val userEmail = binding.loginEmailET.text.toString()
+            val userPassword = binding.loginPasswordET.text.toString()
 
             auth.createUserWithEmailAndPassword(userEmail, userPassword)
                 .addOnCompleteListener(requireActivity()) { task ->
--- app/src/main/java/se/nishiyamastudios/pia11feb02shopping/ShoppingFragment.kt
+++ app/src/main/java/se/nishiyamastudios/pia11feb02shopping/ShoppingFragment.kt
@@ -7,29 +7,51 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import se.nishiyamastudios.pia11feb02shopping.databinding.FragmentShoppingBinding
 
 class ShoppingFragment : Fragment() {
+
+    var _binding : FragmentShoppingBinding? = null //_namn signalerar att vi inte pratar direkt med den.
+    val binding get() = _binding!! //get() = packa upp den, ge mig _binding som null, slipper använda _binding!! överallt.
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping, container, false)
+        //utan binding
+        //return inflater.inflate(R.layout.fragment_shopping, container, false)
+
+        _binding = FragmentShoppingBinding.inflate(inflater, container, false)
+        val view = binding.root //root = botten av vyn, ex. constraintlayouten
+        return view //botten av vyn returneras
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null //tömmer kopplingen när vyn förstörs, för att städa upp.
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        /*
         view.findViewById<Button>(R.id.logoutButton).setOnClickListener {
             Firebase.auth.signOut()
         }
 
-        view.findViewById<Button>(R.id.addShoppingButton).setOnClickListener {
-            val addshopname = view.findViewById<EditText>(R.id.shoppingNameET).text.toString()
+         */
+
+        binding.logoutButton.setOnClickListener {
+            Firebase.auth.signOut()
+        }
+
+        binding.addShoppingButton.setOnClickListener {
+            val addshopname = binding.shoppingNameET.text.toString()
 
             val tempShopitem = ShoppingItem(addshopname)
 
